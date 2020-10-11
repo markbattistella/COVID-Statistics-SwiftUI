afd3286926911271dbc9960ee7eb7b40a13733de
markbattistella <markbattistella@users.noreply.github.com>
2020-10-11 16:42:14 +1100

    Initial Commit

---
+++ COVID stats/COVID_statsApp.swift
@@ -0,0 +1,17 @@
+//
+//  COVID_statsApp.swift
+//  COVID stats
+//
+//  Created by Mark Battistella on 11/10/20.
+//
+
+import SwiftUI
+
+@main
+struct COVID_statsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
